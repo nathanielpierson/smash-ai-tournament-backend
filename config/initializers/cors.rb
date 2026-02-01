44c5b9919f02453c50079fc0cd12dfb120ae1998
nathanielpierson <nathanielepierson@gmail.com>
2026-02-01 17:34:40 -0500

    add extra cors origin location for testing

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -6,6 +6,7 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
     # Update with your frontend URL(s), e.g. http://localhost:5173 for Vite
     origins "http://localhost:3000", "http://127.0.0.1:3000",
             "http://localhost:5173", "http://127.0.0.1:5173",
+            "http://localhost:5174", "http://127.0.0.1:5174",
             "http://localhost:8080", "http://127.0.0.1:8080"
 
     resource "*",
