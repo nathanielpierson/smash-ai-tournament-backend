4d100e30ec0c472d42dd97432b1e1eb3948de811
nathanielpierson <nathanielepierson@gmail.com>
2026-02-24 12:13:22 -0500

    Add cors rules for production

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2026_01_21_173651) do
+ActiveRecord::Schema[8.0].define(version: 2026_02_24_180000) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -21,6 +21,7 @@ ActiveRecord::Schema[8.0].define(version: 2026_01_21_173651) do
     t.integer "slot_number"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "icon_image"
   end
 
   create_table "matches", force: :cascade do |t|
